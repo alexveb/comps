116b998578c94acf83031dcb63ed3ffb58e09cf9
alexveb <verbalex2011@hotmail.com>
2023-02-16 21:57:16 +0200

    button props and classnames

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "./Button";
+
 function App() {
-  return <div>App</div>;
+  const handleClick = () => {
+    console.log("Clicked!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button secondary rounded className="mb-5" onClick={handleClick}>
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Hide Ads!</Button>
+      </div>
+      <div>
+        <Button primary>Something!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
